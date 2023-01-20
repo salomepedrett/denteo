75cee8e1b94a723622f57b83d329ea765a16fa81
fullOfStars <MyGodItsFullOfStars@gmx.ch>
2023-01-20 16:11:02 +0100

    Configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-  ],
-})
+    vue()
+  ],                                                                                     
+  resolve: {                                                                             
+    alias: {                                                                             
+      'vue': path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'),        
+    },                                                                                   
+  }                                                                                      
+})                                                                                       
+
